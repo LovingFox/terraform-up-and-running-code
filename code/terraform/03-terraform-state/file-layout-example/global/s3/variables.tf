2431540477d1b3d9ac0d6ccc6a2a1f6e9948aeba
LovingFox <LovingFox@GMail.com>
2021-09-11 12:48:25 +0300

    03 global

--- code/terraform/03-terraform-state/file-layout-example/global/s3/variables.tf
+++ code/terraform/03-terraform-state/file-layout-example/global/s3/variables.tf
@@ -1,9 +1,11 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket. Must be globally unique."
   type        = string
+  default     = "terraform-asg-bucket-revyakin"
 }
 
 variable "table_name" {
   description = "The name of the DynamoDB table. Must be unique in this AWS account."
   type        = string
+  default     = "terraform-asg-table-revyakin"
 }
